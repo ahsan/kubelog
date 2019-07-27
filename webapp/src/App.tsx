4a3dbb4cb9717c6c134cb6e5bd136f37e14318d3
Abdullah Ahsan <ahsan.abdulah@gmail.com>
2019-07-28 01:38:47 +0200

    Implementation of WS on webapp

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -6,22 +6,62 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
-const App: React.FC = () => {
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <div style={{height: '100vh', display: 'flex', flexDirection: 'column'}}>
-        <div>
-          NavBar
+import WebSocket from 'isomorphic-ws';
+
+type State = {
+  logs: string[];
+}
+
+class App extends React.Component<any, State> {
+  logs: string[] = [];
+
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      logs: []
+    };
+    
+    // const ws = new WebSocket('wss://echo.websocket.org/', {
+    //   origin: 'https://websocket.org'
+    // });
+    const ws = new WebSocket('ws://localhost:8080');
+    
+    ws.onopen = () => {
+      console.log('connected');
+    }
+    
+    ws.onclose = () => {
+      console.log('disconnected');
+    }
+    
+    ws.onmessage = (data: { data: WebSocket.Data; type: string; target: WebSocket }) => {
+      const message = '' + data.data;
+      this.setState({
+        logs: [ ...this.state.logs, message ]
+      });
+      console.log('length: ', this.state.logs.length);
+    }
+  }
+
+
+  render() {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <div style={{height: '100vh', display: 'flex', flexDirection: 'column'}}>
+          <div>
+            NavBar
+          </div>
+          <Container maxWidth="lg" style={{flex: 'auto'}}>
+            <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100%'}}>
+              {this.state.logs.map(log => <div>{log}</div>)}
+            </Typography>
+          </Container>
         </div>
-        <Container maxWidth="lg" style={{flex: 'auto'}}>
-          <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100%'}}>
-            Hello
-          </Typography>
-        </Container>
-      </div>
-    </React.Fragment>  
-  );
+      </React.Fragment>  
+    );
+  }
 }
 
 export default App;
