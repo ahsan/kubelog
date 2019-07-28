523ae217e25511f5cdbdb49b60ee0467581a3069
Abdullah Ahsan <ahsan.abdulah@gmail.com>
2019-07-28 18:23:07 +0200

    Adds a simple navbar (#3)
    * Added bootstrap to the webapp project
    
    * Implemented a simple navbar component

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+
+import Navbar from './components/navbar/navbar'
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
@@ -50,9 +52,7 @@ class App extends React.Component<any, State> {
       <React.Fragment>
         <CssBaseline />
         <div style={{height: '100vh', display: 'flex', flexDirection: 'column'}}>
-          <div>
-            NavBar
-          </div>
+          <Navbar/>
           <Container maxWidth="lg" style={{flex: 'auto'}}>
             <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100%'}}>
               {this.state.logs.map(log => <div>{log}</div>)}
