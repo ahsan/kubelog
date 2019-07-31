d273da7e8b3536ffff9f4875771e913df9f7a8b7
Abdullah Ahsan <ahsan.abdulah@gmail.com>
2019-07-31 21:59:11 +0200

    Implementation of simple table component (#4)
    * Webapp restructuring
    
    * Added simple virtualized table
    
    * row width

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,48 +2,18 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-import Navbar from './components/navbar/navbar'
+import Navbar from './components/navbar/navbar';
+import Body from './components/body';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
-import WebSocket from 'isomorphic-ws';
 
-type State = {
-  logs: string[];
-}
-
-class App extends React.Component<any, State> {
-  logs: string[] = [];
+class App extends React.Component<any> {
 
   constructor(props: any) {
     super(props);
-
-    this.state = {
-      logs: []
-    };
-    
-    // const ws = new WebSocket('wss://echo.websocket.org/', {
-    //   origin: 'https://websocket.org'
-    // });
-    const ws = new WebSocket('ws://localhost:8080');
-    
-    ws.onopen = () => {
-      console.log('connected');
-    }
-    
-    ws.onclose = () => {
-      console.log('disconnected');
-    }
-    
-    ws.onmessage = (data: { data: WebSocket.Data; type: string; target: WebSocket }) => {
-      const message = '' + data.data;
-      this.setState({
-        logs: [ ...this.state.logs, message ]
-      });
-      console.log('length: ', this.state.logs.length);
-    }
   }
 
 
@@ -55,7 +25,7 @@ class App extends React.Component<any, State> {
           <Navbar/>
           <Container maxWidth="lg" style={{flex: 'auto'}}>
             <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100%'}}>
-              {this.state.logs.map(log => <div>{log}</div>)}
+              <Body/>
             </Typography>
           </Container>
         </div>
---
+++ webapp/src/components/logsTable/table.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { VirtualizedTable } from '../virtualized.table';
+import { Paper } from '@material-ui/core';
+
+// TODO: move logs type definition to a common base module, outside of webapp.
+export type Log = {
+  msg: string
+}
+export type LogsTableProps = {
+  logs: Log[];
+}
+
+type LogsTableState = {
+  tableWidth: number;
+}
+
+class LogsTable extends React.Component<LogsTableProps, LogsTableState> {
+  
+  tableContainer: React.RefObject<any>;
+
+  constructor(props: LogsTableProps) {
+    super(props);
+    this.tableContainer = React.createRef();
+    this.state = {
+      tableWidth: 0,
+    };
+  }
+
+  getRow({index}: {index: number}) {
+    const {msg} = this.props.logs[index];
+    console.log(`For ${index}: `, msg);
+    return this.props.logs[index];
+  }
+
+  componentDidMount() {
+    const tableWidth = this.tableContainer.current.clientWidth;
+    this.setState({ tableWidth });
+  }
+
+  render() {
+    console.log(this.props.logs.length);
+    return (
+      <React.Fragment>
+        {
+          <Paper 
+            ref={this.tableContainer}
+            style={{
+              width: '100%',
+              height: '100%',
+              scrollBehavior: 'auto',
+            }}>
+            <VirtualizedTable
+            rowCount={this.props.logs.length}
+            rowGetter={({index}) => this.props.logs[index]}
+            columns={[
+              {
+                width: this.state.tableWidth,
+                label: 'Message',
+                dataKey: 'msg'
+              }
+            ]}
+
+          />
+          </Paper>
+        }
+      </React.Fragment>
+    )
+  }
+}
+
+export default LogsTable;
